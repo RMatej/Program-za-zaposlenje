a678388f52ee6d065a81db889847b0b8fc4b68cb
RMatej <matejrajcic@iMac-od-Matej.local>
2015-11-18 19:00:56 +0100

    Dodan header file za funkcije
    U headeru imena Funkcije ce pisati sve funkcije vezane uz ovaj program
    s potrebnim komentarima kako bi lakse razumjeli njenu zadacu

---
+++ Program za odabir zaposlenika/Program za odabir zaposlenika/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Program za odabir zaposlenika
+//
+//  Created by Matej Rajcic on 18.11.2015..
+//  Copyright (c) 2015. Matej Rajcic. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "FunkcijePrograma.pch"
+using namespace std;
+
+
+int main() {
+    
+}
